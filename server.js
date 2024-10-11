b3d139a25ddceb59be063b46cbd630bb1fcab3d4
Hans0l023 <watts.hans@gmail.com>
2024-10-10 21:20:44 -0400

    fix

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const cors = require('cors')
 const app = express();
 
 const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
 
 app.use(bodyParser.json())
     .use(session({
